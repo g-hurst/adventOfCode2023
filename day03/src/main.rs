e5a103f128894b7c1d26a8c3f2d4cbf13abbd2fe
g <pidgions21@gmail.com>
2023-12-07 23:03:14 -0500

    completed d3 p2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -83,6 +83,60 @@ fn part_1(lines: Vec<String>) -> u32 {
 
 fn part_2(lines: Vec<String>) -> u32 {
     let mut sum: u32= 0;
+
+    // find the loccations of the symbols and numbers
+    let mut is_parsing = false;
+    let mut num:u32 = 0;
+    let mut map_nums:Vec<(u32, Vec<(usize, usize)>)> = Vec::new();
+    let mut coords_syms:Vec<(usize, usize)>            = Vec::new();
+    let mut coords_num = Vec::new();
+    for (i, line) in lines.iter().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                num = num * 10 + c.to_digit(10).unwrap();
+                is_parsing = true;
+                coords_num.push((i, j));
+            }
+            else {
+                if c != '.' {
+                    coords_syms.push((i, j));
+                }
+                if is_parsing {
+                    map_nums.push((num, coords_num.clone()));
+                    num = 0;
+                }
+                coords_num = Vec::new();
+                is_parsing = false;
+            }
+        }
+    }
+
+    // return if a coordinate is adjacent to anythin in the vec
+    fn get_is_adj(a:usize, b:usize, check:&Vec<(usize, usize)>) -> bool {
+        let mut found:bool = false;
+        for (i, j) in check.iter() {
+            found = ((*i as isize - a as isize).abs() < 2) &&
+                ((*j as isize - b as isize).abs() < 2);
+            if found { break; }
+        }
+        found
+    }
+
+    // parse the numbers and find ones next to symbols
+    for (i, j) in coords_syms.iter() {
+        let mut ratio = 1;
+        let mut ratio_num = 0;
+        for (num, v) in map_nums.iter() {
+            if get_is_adj(*i, *j, v) {
+                ratio_num += 1;
+                ratio *= *num;
+            }
+        }
+        if ratio_num > 1 {
+            sum += ratio;
+        }
+    }
+
     sum
 }
 
@@ -96,11 +150,9 @@ fn main() {
     let p1_answer = part_1(input.clone());
     println!("part_1 == {}", p1_answer);
 
-
     // part 2
-    // println!("p2 == {}", part_2(test_input.clone()));
-    // assert!(part_2(test_input.clone()) == 2286);
+    assert!(part_2(test_input.clone()) == 467835);
 
-    // let p2_answer = part_2(input.clone());
-    // println!("part_2 == {}", p2_answer);
+    let p2_answer = part_2(input.clone());
+    println!("part_2 == {}", p2_answer);
 }
